b553d405c23148ef80f4d74e581f99a8fee0175a
jinky0ung <59394430+jinky0ung@users.noreply.github.com>
2020-02-02 23:40:41 +0900

    python hw_end
    python hw_end

---
+++ practice/126.py
@@ -0,0 +1,9 @@
+#126.portfolio에는 보유중인 주식 목록이 저장돼 있다. 아래와 같이 화면에 출력하라.
+portfolio = ["SK하이닉스", "삼성전자", "LG전자"]
+#실행 예
+#SK하이닉스 보유중
+#삼성전자 보유중
+#LG전자 보유중
+
+for ticker in portfolio:
+    print(ticker, "보유중")
---
+++ practice/236.py
@@ -0,0 +1,15 @@
+#236.아래 코드의 실행 결과를 예측하라.
+def message() :
+    print("A")
+    print("B")
+
+message()
+print("C")
+message()
+
+#이해가 안됨(왜 ABC 뒤에 AB가 또 나오는지)
+A
+B
+C
+A
+B
