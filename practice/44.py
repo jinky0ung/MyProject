d54059f600f6862de85ad7ddfaf6cc85f75f02d9
jinky0ung <59394430+jinky0ung@users.noreply.github.com>
2020-02-02 22:21:33 +0900

    python hw
    python hw

---
+++ practice/44.py
@@ -0,0 +1,5 @@
+#044.movie_rank 리스트에서 '럭키'를 삭제하라.
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+#print(movie_rank.remove('럭키'))
+#print(del movie_rank[4])
+
---
+++ practice/46.py
@@ -0,0 +1,10 @@
+#046. lang1과 lang2 리스트가 있을 때 lang1과 lang2의 원소를 모두 갖고 있는 langs 리스트를 만들어라.
+#>> lang1 = ["C", "C++", "JAVA"]
+#>> lang2 = ["Python", "Go", "C#"]
+#실행 예:
+#>> langs
+#['C', 'C++', 'JAVA', 'Python', 'Go', 'C#']
+
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+print(lang1+lang2)
