2f320514dfdc2c7d90504cec05c4311fad400a52
jinky0ung <59394430+jinky0ung@users.noreply.github.com>
2020-02-06 20:10:41 +0900

    added

--- practice/44.py
+++ practice/44.py
@@ -1,5 +1,7 @@
 #044.movie_rank 리스트에서 '럭키'를 삭제하라.
 movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
-#print(movie_rank.remove('럭키'))
-#print(del movie_rank[4])
 
+movie_rank.remove('럭키')
+#del movie_rank[4]
+
+print(movie_rank)
